bd5866b45b6f63635a3019a55fd3a44ffddf1684
Brandon Shar <brandon.shar@icloud.com>
2025-01-05 14:06:47 -0500

    Add merge props

--- inertia/utils.py
+++ inertia/utils.py
@@ -2,6 +2,7 @@ from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 from django.db.models.query import QuerySet
 from django.forms.models import model_to_dict as base_model_to_dict
+from .prop_classes import OptionalProp, DeferredProp, MergeProp
 import warnings
 
 def model_to_dict(model):
@@ -17,35 +18,19 @@ class InertiaJsonEncoder(DjangoJSONEncoder):
     
     return super().default(value)
 
-class LazyProp:
-  def __init__(self, prop):
-    warnings.warn(
-      "lazy and LazyProp are deprecated and will be removed in a future version. Please use optional instead.",
-      DeprecationWarning,
-      stacklevel=2
-    )
-    self.prop = prop
-
-  def __call__(self):
-    return self.prop() if callable(self.prop) else self.prop
-
-class OptionalProp(LazyProp):
-  def __init__(self, prop):
-    self.prop = prop
-  
-class DeferredProp:
-  def __init__(self, prop, group):
-    self.prop = prop
-    self.group = group
-
-  def __call__(self):
-    return self.prop() if callable(self.prop) else self.prop
-
 def lazy(prop):
-  return LazyProp(prop)
+  warnings.warn(
+    "lazy is deprecated and will be removed in a future version. Please use optional instead.",
+    DeprecationWarning,
+    stacklevel=2
+  )
+  return optional(prop)
 
 def optional(prop):
   return OptionalProp(prop)
 
-def defer(prop, group="default"):
-  return DeferredProp(prop, group)
+def defer(prop, group="default", merge=False):
+  return DeferredProp(prop, group=group, merge=merge)
+
+def merge(prop):
+  return MergeProp(prop)
