dacab442d89eff81871c26927be05286cfd8bbe4
Sven Groot <sven@wearemoose.com>
2025-01-09 09:45:57 +0100

    Fix unused import F401

--- inertia/__init__.py
+++ inertia/__init__.py
@@ -1,3 +1,15 @@
 from .http import InertiaResponse, inertia, location, render
 from .share import share
 from .utils import defer, lazy, merge, optional
+
+__all__ = [
+    "InertiaResponse",
+    "inertia",
+    "location",
+    "render",
+    "share",
+    "defer",
+    "lazy",
+    "merge",
+    "optional",
+]
