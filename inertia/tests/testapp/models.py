f27804b85bebd7be1891761e97c5a26f2767a63d
Brandon Shar <brandon.shar@icloud.com>
2025-01-26 20:10:00 -0500

    Add InertiaMeta for automatic serialization

--- inertia/tests/testapp/models.py
+++ inertia/tests/testapp/models.py
@@ -6,3 +6,12 @@ class User(models.Model):
     password = models.CharField(max_length=255)
     birthdate = models.DateField()
     registered_at = models.DateTimeField()
+    created_at = models.DateField(auto_now_add=True)
+
+class Sport(models.Model):
+    name = models.CharField(max_length=255)
+    season = models.CharField(max_length=255)
+    created_at = models.DateField(auto_now_add=True)
+
+    class InertiaMeta:
+        fields = ('id', 'name', 'created_at')
