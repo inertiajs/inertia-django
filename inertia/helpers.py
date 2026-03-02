dfe7c5146ad0928a0c432f7dcc5285a114569571
Jesse Sopel <jesse@sopelj.ca>
2026-03-01 20:44:53 -0500

    feat: Add more advanced types

--- inertia/helpers.py
+++ inertia/helpers.py
@@ -1,9 +1,17 @@
-from typing import Any
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, cast
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+    from typing import Any, TypeVar
+
+    T = TypeVar("T", bound=Any)
 
 
-def deep_transform_callables(prop: Any) -> Any:
+def deep_transform_callables(prop: T | Callable[[], T]) -> T:
     if not isinstance(prop, dict):
-        return prop() if callable(prop) else prop
+        return cast("T", prop() if callable(prop) else prop)
 
     for key in list(prop.keys()):
         prop[key] = deep_transform_callables(prop[key])
--- inertia/prop_classes.py
+++ inertia/prop_classes.py
@@ -1,13 +1,21 @@
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import TYPE_CHECKING, Any, Generic, TypeVar, cast
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
 
 
-class CallableProp:
-    def __init__(self, prop: Any) -> None:
+T = TypeVar("T", bound=Any)
+
+
+class CallableProp(Generic[T]):
+    def __init__(self, prop: T | Callable[[], T]) -> None:
         self.prop = prop
 
-    def __call__(self) -> Any:
-        return self.prop() if callable(self.prop) else self.prop
+    def __call__(self) -> T:
+        return cast(T, self.prop() if callable(self.prop) else self.prop)
 
 
 class MergeableProp(ABC):
@@ -20,12 +28,12 @@ class IgnoreOnFirstLoadProp:
     pass
 
 
-class OptionalProp(CallableProp, IgnoreOnFirstLoadProp):
+class OptionalProp(CallableProp[T], IgnoreOnFirstLoadProp):
     pass
 
 
-class DeferredProp(CallableProp, MergeableProp, IgnoreOnFirstLoadProp):
-    def __init__(self, prop: Any, group: str, merge: bool = False) -> None:
+class DeferredProp(CallableProp[T], MergeableProp, IgnoreOnFirstLoadProp):
+    def __init__(self, prop: T | Callable[[], T], group: str, merge: bool = False) -> None:
         super().__init__(prop)
         self.group = group
         self.merge = merge
@@ -34,6 +42,6 @@ class DeferredProp(CallableProp, MergeableProp, IgnoreOnFirstLoadProp):
         return self.merge
 
 
-class MergeProp(CallableProp, MergeableProp):
+class MergeProp(CallableProp[T], MergeableProp):
     def should_merge(self) -> bool:
         return True
